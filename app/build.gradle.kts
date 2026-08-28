6e94a86dde0ca2cacb3f604b0872257f981a4272
shubh72010 <shub@jusdots.dev>
2026-08-28 13:09:37 +0530

    chore: bump to 0.3.3 (code 7) for ComponentInfo fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.jusdots.jusicons"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.3.2"
+        versionCode = 7
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
